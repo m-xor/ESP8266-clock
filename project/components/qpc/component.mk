3556f482be3ec20c8a695b244b7d15b777253164
xor <wiw@linux.pl>
2020-01-06 15:07:42 +0100

    Refining configuration for QPC use

--- project/components/qpc/component.mk
+++ project/components/qpc/component.mk
@@ -6,16 +6,21 @@
 #
 # Component Makefile
 #
-COMPONENT_ADD_INCLUDEDIRS += include \
-                             src \
-                             app \
-                             freertos
+
+# In components/qpc create symlink 'qpc' to the main QPC directiory
+# Copy files in $(QPC_PATH)/ports/freertos directory to components/qpc/port 
+#   (QF_MAX_ACTIVE needs adjusting to be in sync with FreeRTOS' configMAX_PRIORITIES)
+COMPONENT_ADD_INCLUDEDIRS += app \
+                             qpc/include \
+                             qpc/src \
+                             port
 
 COMPONENT_SRCDIRS += app \
-                     freertos \
-                     include \
-                     src/qf 
+                     qpc/include \
+                     qpc/src/qf \
+                     port
 
 ifdef CONFIG_USING_QS
-COMPONENT_SRCDIRS += src/qs
+COMPONENT_SRCDIRS += qpc/src/qs
+CPPFLAGS += -DQ_SPY
 endif
