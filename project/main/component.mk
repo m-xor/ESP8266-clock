425b0efbf5b0d77e5814294f1740df15a8a49371
xor <wiw@linux.pl>
2020-01-09 20:05:14 +0100

    Binary file containing Web Api files removed from repository

--- project/main/component.mk
+++ project/main/component.mk
@@ -2,3 +2,10 @@
 # "main" pseudo-component makefile.
 #
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+# updating fsdata.h file which constitutes ROM filesystem for web api files
+fs.o: fsdata.h
+fsdata.h: $(PROJECT_PATH)/../html/*.*
+	cd $(PROJECT_PATH)/.. ; \
+          $(QPT_PATH)/qfsgen html $(COMPONENT_BUILD_DIR)/fsdata.h; \
+          cd $(COMPONENT_BUILD_DIR)
